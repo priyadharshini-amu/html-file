dfa679efc176b559aad4fcce4c6ac95ca551bde3
priyadharshini-amu <dharshinidharshika116@gmail.com>
2026-07-09 09:21:19 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Demo{
+    public static void main(String[]args){
+        String name="jayashree";
+        int age=21;
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
